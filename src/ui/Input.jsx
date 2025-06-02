e7d047269e71d5ce50b3b01d928df1f355037678
qa3emnik <codewithqa3em@gmail.com>
2025-06-02 09:37:48 +0430

    create input styled component UI

---
+++ src/ui/Input.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const Input = styled.input`
+  padding: 1.2rem 1.6rem;
+  border: 1px solid var(--color-brand-800);
+  border-radius: var(--border-radius-sm);
+  margin-right: 1.6rem;
+`;
+export default Input;
