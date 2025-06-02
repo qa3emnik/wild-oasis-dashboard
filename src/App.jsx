3be1f54bcb3e75ab5d8b6f759046574931af15fa
qa3emnik <codewithqa3em@gmail.com>
2025-06-02 17:49:09 +0430

    update query client stale time for queries

--- src/App.jsx
+++ src/App.jsx
@@ -17,8 +17,8 @@ import { Toaster } from "react-hot-toast";
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      // staleTime: 60 * 1000,
-      staleTime: 0,
+      staleTime: 600 * 1000,
+      // staleTime: 0,
     },
   },
 });
