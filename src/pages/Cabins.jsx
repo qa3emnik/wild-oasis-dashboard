427fcc3431663181d532cb0967f17da8edf2893f
qa3emnik <codewithqa3em@gmail.com>
2025-06-03 16:34:08 +0430

    create Cabin page

---
+++ src/pages/Cabins.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import CabinTable from "../features/cabins/CabinTable";
+import Button from "../ui/Button";
+import Heading from "../ui/Heading";
+import Row from "../ui/Row";
+import CreateCabinForm from "../features/cabins/CreateCabinForm";
+
+function Cabins() {
+  const [showForm, setShowForm] = useState(false);
+
+  return (
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">All cabins</Heading>
+        <p>filter / sort</p>
+      </Row>
+      <Row type="vertical">
+        <CabinTable />
+        <Button
+          $variation="primary"
+          $size="large"
+          onClick={() => setShowForm((show) => !show)}
+        >
+          Add new Cabin
+        </Button>
+        {showForm && <CreateCabinForm />}
+      </Row>
+    </>
+  );
+}
+
+export default Cabins;
