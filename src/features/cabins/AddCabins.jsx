f5b4d363850bc9d2dda33127a6dee2261e3cd11e
qa3emnik <codewithqa3em@gmail.com>
2025-06-14 15:48:05 +0430

    Feat: using compound component

--- src/features/cabins/AddCabins.jsx
+++ src/features/cabins/AddCabins.jsx
@@ -1,3 +1,28 @@
+import CreateEditCabinForm from "./CreateCabinForm";
+import Button from "../../ui/Button";
+import Modal from "../../ui/Modal";
+import CabinTable from "./CabinTable";
+
 export default function AddCabins() {
-  return <div>AddCabins</div>;
+  return (
+    <Modal>
+      <Modal.Open opens="cabin-form">
+        <Button $variation="primary" $size="large">
+          Add new Cabin
+        </Button>
+      </Modal.Open>
+      <Modal.Window name="cabin-form">
+        <CreateEditCabinForm />
+      </Modal.Window>
+
+      <Modal.Open opens="table">
+        <Button $variation="primary" $size="large">
+          Show Table
+        </Button>
+      </Modal.Open>
+      <Modal.Window name="table">
+        <CabinTable />
+      </Modal.Window>
+    </Modal>
+  );
 }
