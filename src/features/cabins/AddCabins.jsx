ba2d3d5e0a9aea8fbcc3bd051e57359b2d271569
qa3emnik <codewithqa3em@gmail.com>
2025-06-15 14:55:51 +0430

    Feat: add modal for table

--- src/features/cabins/AddCabins.jsx
+++ src/features/cabins/AddCabins.jsx
@@ -6,22 +6,15 @@ import CabinTable from "./CabinTable";
 export default function AddCabins() {
   return (
     <Modal>
-      <Modal.Open opens="cabin-form">
-        <Button $variation="primary" $size="large">
-          Add new Cabin
-        </Button>
-      </Modal.Open>
+      <div>
+        <Modal.Open opens="cabin-form">
+          <Button $variation="primary" $size="large">
+            Add new Cabin
+          </Button>
+        </Modal.Open>
+      </div>
       <Modal.Window name="cabin-form">
         <CreateEditCabinForm />
-      </Modal.Window>
-
-      <Modal.Open opens="table">
-        <Button $variation="primary" $size="large">
-          Show Table
-        </Button>
-      </Modal.Open>
-      <Modal.Window name="table">
-        <CabinTable />
       </Modal.Window>
     </Modal>
   );
