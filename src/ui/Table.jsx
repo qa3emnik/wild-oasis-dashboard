9d838fd5263bfc182efc6f7b7b83ffcc1c9babbe
qa3emnik <codewithqa3em@gmail.com>
2025-06-15 14:59:08 +0430

    create table context

--- src/ui/Table.jsx
+++ src/ui/Table.jsx
@@ -1,3 +1,4 @@
+import { createContext, useContext } from "react";
 import styled from "styled-components";
 
 const StyledTable = styled.div`
@@ -58,3 +59,37 @@ const Empty = styled.p`
   text-align: center;
   margin: 2.4rem;
 `;
+
+const TableContext = createContext();
+
+function Table({ children, columns }) {
+  return (
+    <TableContext.Provider value={{ columns }}>
+      <StyledTable role="table">{children}</StyledTable>
+    </TableContext.Provider>
+  );
+}
+function Body({ data, render }) {
+  if (!data.length) return <Empty>No Data at the Moment</Empty>;
+  return <StyledBody>{data.map(render)}</StyledBody>;
+}
+function Header({ children }) {
+  const { columns } = useContext(TableContext);
+  return (
+    <StyledHeader role="row" as="header" columns={columns}>
+      {children}
+    </StyledHeader>
+  );
+}
+
+function Row({ children }) {
+  const { columns } = useContext(TableContext);
+  return <StyledRow columns={columns}>{children}</StyledRow>;
+}
+
+Table.Body = Body;
+Table.Header = Header;
+Table.Row = Row;
+Table.Footer = Footer;
+
+export default Table;
