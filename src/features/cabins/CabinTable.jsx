3bd992521289f05f16c297c1b19de557d96b9f84
qa3emnik <codewithqa3em@gmail.com>
2025-06-26 16:59:43 +0430

    Func: get params for filtering table data

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -1,29 +1,23 @@
-import styled from "styled-components";
-
 import Spinner from "../../ui/Spinner";
 import CabinRow from "./CabinRow";
 import useCabins from "./useCabins";
 import Table from "../../ui/Table";
 import Menus from "../../ui/Menus";
-
-const TableHeader = styled.header`
-  display: grid;
-  grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
-  column-gap: 2.4rem;
-  align-items: center;
-
-  background-color: var(--color-grey-50);
-  border-bottom: 1px solid var(--color-grey-100);
-  text-transform: uppercase;
-  letter-spacing: 0.4px;
-  font-weight: 600;
-  color: var(--color-grey-600);
-  padding: 1.6rem 2.4rem;
-`;
+import { useSearchParams } from "react-router-dom";
 
 function CabinTable() {
+  const [searchParams] = useSearchParams();
+  const filteredValue = searchParams.get("discount") || "all";
+
   const { isLoading, cabins } = useCabins();
   if (isLoading) return <Spinner />;
+
+  let filteredCabin;
+  if (filteredValue === "all") filteredCabin = cabins;
+  if (filteredValue === "no-discount")
+    filteredCabin = cabins.filter((cabin) => cabin.discount === 0);
+  if (filteredValue === "with-discount")
+    filteredCabin = cabins.filter((cabin) => cabin.discount > 0);
 
   return (
     <Menus>
@@ -37,7 +31,8 @@ function CabinTable() {
           <div></div>
         </Table.Header>
         <Table.Body
-          data={cabins}
+          // data={cabins}
+          data={filteredCabin}
           render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />}
         />
       </Table>
